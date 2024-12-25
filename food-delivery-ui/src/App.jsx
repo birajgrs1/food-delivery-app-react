2a4d8f19ff30a4c9b3c0e7534066b6ea64f80808
birajgrs <birajghorasaine11@gmail.com>
2024-12-25 15:56:11 +0545

    food ordering project in react started: project setup successfully: project title---> DalBhatBox

---
+++ food-delivery-ui/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+
+  return (
+    <>
+    <h2>Welcome to my Website!</h2>
+    </>
+  )
+}
+
+export default App
